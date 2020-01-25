bafedc91301972aff7f604d208148213c29ca3dc
makoto <16ec068@gmail.com>
2020-01-25 19:15:41 +0900

    損失関数を作成

---
+++ net/loss.py
@@ -0,0 +1,47 @@
+import numpy as np
+from keras.applications import vgg19
+from keras import backend as K
+
+target_image = K.constant(preprocess_image(target_image_path))
+style_reference_image = K.constant(preprocess_image(style_reference_image_path))
+
+# This placeholder will contain our generated image
+combination_image = K.placeholder((1, img_height, img_width, 3))
+
+# We combine the 3 images into a single batch
+input_tensor = K.concatenate([target_image,
+                              style_reference_image,
+                              combination_image], axis=0)
+
+# We build the VGG19 network with our batch of 3 images as input.
+# The model will be loaded with pre-trained ImageNet weights.
+model = vgg19.VGG19(input_tensor=input_tensor,
+                    weights='imagenet',
+                    include_top=False)
+print('Model loaded.')
+
+#content loss
+def content_loss(base, combination):
+    return K.sum(K.square(combination - base))
+
+#style_loss
+def gram_matrix(x):
+    features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+    gram = K.dot(features, K.transpose(features))
+    return gram
+
+
+def style_loss(style, combination):
+    S = gram_matrix(style)
+    C = gram_matrix(combination)
+    channels = 3
+    size = img_height * img_width
+    return K.sum(K.square(S - C)) / (4. * (channels ** 2) * (size ** 2))
+
+#total variation loss
+def total_variation_loss(x):
+    a = K.square(
+        x[:, :img_height - 1, :img_width - 1, :] - x[:, 1:, :img_width - 1, :])
+    b = K.square(
+        x[:, :img_height - 1, :img_width - 1, :] - x[:, :img_height - 1, 1:, :])
+    return K.sum(K.pow(a + b, 1.25))
